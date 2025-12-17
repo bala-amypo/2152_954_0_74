154e62b602605f39641b1541f25c82992dc591ad
bala-amypo <bala@amypo.in>
2025-12-17 04:28:49 +0000

    User commit

--- src/main/java/com/example/demo/Controller/Studentcontroller.java
+++ src/main/java/com/example/demo/Controller/Studentcontroller.java
@@ -1,4 +1,4 @@
-package com.example.student.controller;
+package com.example.demo.controller;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,8 +8,18 @@ import org.springframework.wed.bind.annotation.DeleteMapping;
 import org.springframework.wed.bind.annotation.GetMapping;
 import org.springframework.wed.bind.annotation.PathVariable;
 import org.springframework.wed.bind.annotation.PostMapping;
-import org.springframework.wed.bi
+import org.springframework.wed.bind.annotation.PutMapping;
+import org.springframework.wed.bind.annotation.RequestBody;
+import org.springframework.wed.bind.annotation.RestController;
 
-
+import com.example.demo.Entity.Studententity;
+import com.example.demo.Server.Studentservice;
 
 @RestController
+public class Studentcontroller{
+
+    @Autowired
+    Studentservice studentService;
+
+    @
+}
