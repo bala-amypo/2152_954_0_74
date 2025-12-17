575dfe488fbd3870eb3cf3925a5e2a2ab9aeeeda
bala-amypo <bala@amypo.in>
2025-12-17 06:12:12 +0000

    User commit

--- src/main/java/com/example/demo/Service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/Service/Impl/StudentServiceImpl.java
@@ -1,1 +1,23 @@
-packagecom.example.demo.ser
+package com.example.demo.service.impl;
+
+import java.util.*;
+import org.springframework.sterotype.Service;
+import com.example.demo.Studententity.Student;
+
+@Service
+public class StudentServiceImpl implements Studentservice{
+    private final Map<Long,Student> store=new HashMap<>();
+    private long counter=1;
+
+    @Override
+    public Student insertStudent(Student st){
+        st.setId(counter++);
+        store.put(st.getId(),st);
+        return st;
+    }
+
+    @Override
+    public List<Student> getAllStudents(){
+        return newArrayList<>(store.values());
+    }
+}
