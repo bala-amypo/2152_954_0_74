f2767b134d17402a896866caf812968be7fac69a
bala-amypo <bala@amypo.in>
2025-12-17 04:23:48 +0000

    User commit

---
+++ src/main/java/com/example/demo/Controller/Studentcontroller.java
@@ -0,0 +1,15 @@
+package com.example.student.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.wed.bind.annotation.DeleteMapping;
+import org.springframework.wed.bind.annotation.GetMapping;
+import org.springframework.wed.bind.annotation.PathVariable;
+import org.springframework.wed.bind.annotation.PostMapping;
+import org.springframework.wed.bi
+
+
+
+@RestController
