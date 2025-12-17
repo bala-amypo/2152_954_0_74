9f4777d168ffefbdaef83a31141dcb80dcc45f6d
bala-amypo <bala@amypo.in>
2025-12-17 04:48:49 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Studententity.java
+++ src/main/java/com/example/demo/Entity/Studententity.java
@@ -1,3 +1,30 @@
 package com.example.demo.entity;
 
-import java.time.LocalDate
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Studententity{
+    @Id
+    @GeneratedValue(strategy=GeneratedType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Float cgpa;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id=id;
+    }
+
+    public S
+}
